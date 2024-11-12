d97c7b7cac4e76ad419ce7701aa8bccf85d61664
Rouzok <98026745+Rouzok@users.noreply.github.com>
2024-11-13 01:13:26 +0800

    [Update]: Variable Name Change

--- aes-256-cbc.php
+++ aes-256-cbc.php
@@ -1,30 +1,30 @@
 <?php
-function test_encrypt($string, $secret_iv, $secret_key)
+function encrypt($string, $secret_iv, $public_key)
 {
     $encrypt_method = "AES-256-CBC";
-    $key = hash('sha256', $secret_key);
+    $key = hash('sha256', $public_key);
     $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
     $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
     $output =  base64_encode($output);
     return $output;
 }
 
-function test_decrypt($string, $secret_iv, $secret_key)
+function decrypt($string, $secret_iv, $public_key)
 {
     $encrypt_method = "AES-256-CBC";
-    $key = hash('sha256', $secret_key);
+    $key = hash('sha256', $public_key);
     $iv = substr(hash('sha256', $secret_iv), 0, 16);
 
     $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
-
     return $output;
 }
 $message = "Hello";
 $public_key = "user_1";
-$secret_key = "132";
+$secret_iv = "132";
 
-$encrypt_str = test_encrypt($message, $secret_key, $public_key);
+$encrypt_str = encrypt($message, $secret_iv, $public_key);
 echo $encrypt_str . '\n';
 
-$decrypt_str = test_decrypt($encrypt_str, $secret_key, $public_key);
+$decrypt_str = decrypt($encrypt_str, $secret_iv, $public_key);
 echo $decrypt_str;
